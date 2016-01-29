7122e84ef19f5fe29a5e7f15ffaf01039f808ea0
mgsolid543 <irpan.budiana@gmail.com>
2016-01-29 16:28:42 +0700

    customize join table many to manu Sesi <-> Peserta

--- src/main/java/com/budiana/irpan/belajar/entity/Sesi.java
+++ src/main/java/com/budiana/irpan/belajar/entity/Sesi.java
@@ -8,11 +8,11 @@ package com.budiana.irpan.belajar.entity;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
 import javax.persistence.Temporal;
@@ -40,5 +40,10 @@ public class Sesi {
     private Materi materi;
     
     @ManyToMany
+    @JoinTable(
+            name = "peserta_pelatihan",
+            joinColumns = @JoinColumn(name = "id_sesi"),
+            inverseJoinColumns = @JoinColumn(name="id_peserta")
+    )
     private List<Peserta> daftarPeserta = new ArrayList<>();
 }
