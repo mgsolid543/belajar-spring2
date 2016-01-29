b5b7bf6fa117bb003c0b39a88d1f8599d94e116e
mgsolid543 <irpan.budiana@gmail.com>
2016-01-29 17:16:33 +0700

    menambah relasi OneToMany di Materi, Materi -> Sesi

--- src/main/java/com/budiana/irpan/belajar/entity/Materi.java
+++ src/main/java/com/budiana/irpan/belajar/entity/Materi.java
@@ -5,25 +5,40 @@
  */
 package com.budiana.irpan.belajar.entity;
 
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import org.hibernate.annotations.GenericGenerator;
+
 /**
  *
  * @author user
  */
-@Entity @Table(name = "m_materi")
+@Entity
+@Table(name = "m_materi")
 public class Materi {
-    @Id @GeneratedValue(generator="uuid")
-    @GenericGenerator(name="uuid", strategy = "uuid2")
+
+    @Id
+    @GeneratedValue(generator = "uuid")
+    @GenericGenerator(name = "uuid", strategy = "uuid2")
     private String id;
-    
-    @Column(nullable=false, unique = true, length = 10)
+
+    @Column(nullable = false, unique = true, length = 10)
     private String kode;
-    
+
     @Column(nullable = false)
-    private String nama; 
+    private String nama;
+
+    @OneToMany(
+            cascade = CascadeType.ALL,
+            orphanRemoval = true,
+            mappedBy = "materi"
+    )
+    private List<Sesi> daftarSesi = new ArrayList<>();
 }
