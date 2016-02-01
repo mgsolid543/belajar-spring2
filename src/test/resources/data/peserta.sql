265bd61c1d62dccd53a2115086faeb38268e32db
mgsolid543 <irpan.budiana@gmail.com>
2016-02-01 13:54:34 +0700

    test sample data peserta

---
+++ src/test/resources/data/peserta.sql
@@ -0,0 +1,10 @@
+delete from peserta;
+
+insert into peserta (id, nama, email, tanggal_lahir)
+values ('a1', 'Peserta Test 1', 'peserta.test1@gmail.com', '1980-1-1');
+
+insert into peserta (id, nama, email, tanggal_lahir)
+values ('a2', 'Peserta Test 2', 'peserta.test2@gmail.com', '1980-1-2');
+
+insert into peserta (id, nama, email, tanggal_lahir)
+values ('a3', 'Peserta Test 3', 'peserta.test3@gmail.com', '1980-1-3');
