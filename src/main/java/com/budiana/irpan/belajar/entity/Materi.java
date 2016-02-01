f097b53c29a8743bf9e8e04b1620d6f703ca26c1
mgsolid543 <irpan.budiana@gmail.com>
2016-02-01 16:25:32 +0700

    getter dan setter untuk Materi dan Sesi

--- src/main/java/com/budiana/irpan/belajar/entity/Materi.java
+++ src/main/java/com/budiana/irpan/belajar/entity/Materi.java
@@ -21,7 +21,7 @@ import org.hibernate.annotations.GenericGenerator;
  * @author user
  */
 @Entity
-@Table(name = "m_materi")
+//@Table(name = "m_materi")
 public class Materi {
 
     @Id
@@ -41,4 +41,38 @@ public class Materi {
         mappedBy = "materi"
     )
     private List<Sesi> daftarSesi = new ArrayList<>();
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getKode() {
+        return kode;
+    }
+
+    public void setKode(String kode) {
+        this.kode = kode;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public List<Sesi> getDaftarSesi() {
+        return daftarSesi;
+    }
+
+    public void setDaftarSesi(List<Sesi> daftarSesi) {
+        this.daftarSesi = daftarSesi;
+    }
+    
+    
 }
