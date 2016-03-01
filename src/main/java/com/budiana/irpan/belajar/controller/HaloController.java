583c940f629a083289389326f46f0e92a2d0659f
mgsolid543 <irpan.budiana@gmail.com>
2016-03-01 14:20:55 +0700

    HaloController with Param

--- src/main/java/com/budiana/irpan/belajar/controller/HaloController.java
+++ src/main/java/com/budiana/irpan/belajar/controller/HaloController.java
@@ -9,6 +9,7 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -19,8 +20,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class HaloController {
     
     @RequestMapping("/halo")
-    public Map<String, Object> halo() {
-        Map<String, Object> hasil = new HashMap<>();
+    public Map<String, Object> halo(
+            @RequestParam(value="nama", required=false) String nama) {
+        Map<String, Object> hasil = new HashMap<>(); 
+        hasil.put("nama", nama);
         hasil.put("waktu", new Date());
         return hasil;
     }
