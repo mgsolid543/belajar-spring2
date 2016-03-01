d11c8274790929ab66814d0366734afb2624d2c3
mgsolid543 <irpan.budiana@gmail.com>
2016-03-01 10:53:04 +0700

    Add HaloController

---
+++ src/main/java/com/budiana/irpan/belajar/controller/HaloController.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.budiana.irpan.belajar.controller;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author Irpan Budiana
+ */
+@RestController
+public class HaloController {
+    
+    @RequestMapping("/halo")
+    public Map<String, Object> halo() {
+        Map<String, Object> hasil = new HashMap<>();
+        hasil.put("waktu", new Date());
+        return hasil;
+    }
+}
