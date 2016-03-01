7d2c14cf8205bb01663f3f2b8e827d3ec7f5dcdc
mgsolid543 <irpan.budiana@gmail.com>
2016-03-01 16:18:08 +0700

    Peserta Controller, Add: - GetAllPeserta - UpdatePeserta - CariPesertaById

--- src/main/java/com/budiana/irpan/belajar/controller/PesertaController.java
+++ src/main/java/com/budiana/irpan/belajar/controller/PesertaController.java
@@ -10,7 +10,12 @@ import com.budiana.irpan.belajar.entity.Peserta;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -23,8 +28,28 @@ public class PesertaController {
     @Autowired
     private PesertaDao pd;
     
-    @RequestMapping("/peserta")
+    @RequestMapping(value="/peserta", method = RequestMethod.GET)
     public Page<Peserta> cariPeserta(Pageable page) {
         return pd.findAll(page);
-    } 
+    }
+    
+    @RequestMapping(value="/peserta", method = RequestMethod.POST)
+    @ResponseStatus(HttpStatus.CREATED)
+    public void insertPesertaBaru(@RequestBody  Peserta p) {
+        pd.save(p);
+    }
+    
+    @RequestMapping(value="/peserta/{id}", method = RequestMethod.PUT)
+    @ResponseStatus(HttpStatus.OK)
+    public void updatePeserta(@PathVariable("id") String id, @RequestBody Peserta p) {
+        p.setId(id);
+        pd.save(p);
+    }
+    
+    @RequestMapping(value="/peserta/{id}", method = RequestMethod.GET)
+    @ResponseStatus(HttpStatus.OK)
+    public Peserta cariPesertaById(@PathVariable("id") String id) {
+        return pd.findOne(id);
+    }
+    
 }
