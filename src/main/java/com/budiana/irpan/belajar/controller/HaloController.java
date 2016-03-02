9428e603ef5cc7db40d2ba6839d66ad470158483
mgsolid543 <irpan.budiana@gmail.com>
2016-03-02 15:03:13 +0700

    templating halo

--- src/main/java/com/budiana/irpan/belajar/controller/HaloController.java
+++ src/main/java/com/budiana/irpan/belajar/controller/HaloController.java
@@ -8,23 +8,32 @@ package com.budiana.irpan.belajar.controller;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  *
  * @author Irpan Budiana
  */
-@RestController
+@Controller
 public class HaloController {
     
-    @RequestMapping("/halo")
+    @RequestMapping("/halorest")
+    @ResponseBody
     public Map<String, Object> halo(
             @RequestParam(value="nama", required=false) String nama) {
         Map<String, Object> hasil = new HashMap<>(); 
         hasil.put("nama", nama);
-        hasil.put("waktu", new Date());
+        hasil.put("waktu", new Date()); 
         return hasil;
+    }
+    
+    @RequestMapping("/halo")
+    public void haloHtml(@RequestParam(value="nama", required=false) String nama, Model hasil) {
+        hasil.addAttribute("nama", nama);
+        hasil.addAttribute("waktu", new Date());
     }
 }
